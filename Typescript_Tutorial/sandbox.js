228139b31d01e7d48a89b0b95e408036db4f7352
Pedro López <plopez@financialforce.com>
2023-03-01 12:01:13 +0100

    Lesson 5

--- Typescript_Tutorial/sandbox.js
+++ Typescript_Tutorial/sandbox.js
@@ -1,7 +1,16 @@
-var names = ['mario', 'luigi', 'yoshi'];
-names.push('bowser');
-var ninja = {
-    name: 'mario',
-    belt: 'black',
-    age: 15
-};
+// explicit
+var var1;
+var var2;
+var var3;
+// arrays
+var stringArray = [];
+// mixed
+var mixedArray = [];
+mixedArray.push('mario');
+mixedArray.push(20);
+var mixedVar = '20';
+mixedVar = 20;
+// object
+var ninjaOne;
+var ninjaTwo;
+ninjaTwo = { name: 'Pedro', age: 5 };
--- Typescript_Tutorial/sandbox.ts
+++ Typescript_Tutorial/sandbox.ts
@@ -1,8 +1,24 @@
-let names = [ 'mario', 'luigi', 'yoshi'];
-names.push('bowser');
+// explicit
+let var1: string;
+let var2: number;
+let var3: boolean;
 
-let ninja = {
-    name: 'mario',
-    belt: 'black',
-    age: 15
+// arrays
+let stringArray: string[] = [];
+
+// mixed
+let mixedArray: (string|number)[] = [];
+mixedArray.push('mario');
+mixedArray.push(20);
+
+let mixedVar: string|number = '20';
+mixedVar = 20;
+
+// object
+let ninjaOne: object;
+let ninjaTwo: {
+    name: string,
+    age: number
 };
+
+ninjaTwo = { name:'Pedro', age:5}
