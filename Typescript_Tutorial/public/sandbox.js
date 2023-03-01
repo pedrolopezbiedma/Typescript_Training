99ac40abe0321eabe39361802ad46d60d4523c5a
Pedro López <plopez@financialforce.com>
2023-03-01 12:32:57 +0100

    Lesson 8

--- Typescript_Tutorial/public/sandbox.js
+++ Typescript_Tutorial/public/sandbox.js
@@ -5,4 +5,7 @@ let greet = () => {
 const add = (a, b = 10, c) => {
     console.log(a + b);
 };
+const multiply = (a, b = 10) => {
+    return a * b;
+};
 add(5, 8);
--- Typescript_Tutorial/src/sandbox.ts
+++ Typescript_Tutorial/src/sandbox.ts
@@ -2,8 +2,12 @@ let greet = () => {
     console.log('Hello');
 }
 
-const add = (a:number, b:number = 10, c?: number|string) => {
+const add = (a:number, b:number = 10, c?: number|string): void => {
     console.log(a+b);
+}
+
+const multiply = (a:number, b:number = 10): number => {
+    return a*b;
 }
 
 add(5,8);
